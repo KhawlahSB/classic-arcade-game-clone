f0f84b6281e7385364b8f6341a4a3044351c7c1b
KhawlahSB <khawlahsb@gmail.com>
2017-04-10 04:03:57 +0300

    ready for submission

--- js/app.js
+++ js/app.js
@@ -1,19 +1,25 @@
 'use strict';
-// Enemies 
+// Enemies our player must avoid
 var Enemy = function(initialX, initialY, speed) {
-   
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
     this.x = initialX;
     this.y = initialY;
     this.speed = speed;
-    
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
     this.x = this.x + this.speed * dt;
     if (this.x > 500) {
+        // Initial enemy x-axis position
         this.x = -60;
         this.randomSpeed();
     }
@@ -27,7 +33,7 @@ Enemy.prototype.update = function(dt) {
     }
 }
 
-
+// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
@@ -36,13 +42,17 @@ Enemy.prototype.randomSpeed = function() {
     this.speed = 75 * speedMultiply;
 }
 
-// player
+// Now write your own player class
+// This class requires an update(), render() and
+// a handleInput() method.
 var playerInitialX = 200,
     playerInitialY = 400;
 
-
+// Now instantiate your objects.
+// Place all enemy objects in an array called allEnemies
+// Place the player object in a variable called player
 var Player = function() {
-  
+    // Play initial place coordinate
     this.x = playerInitialX;
     this.y = playerInitialY;
     this.wallChecker = {
@@ -53,12 +63,12 @@ var Player = function() {
     this.sprite = 'images/char-boy.png';
 }
 
-
+// Player class instance methods ✓ (EMPTY)
 Player.prototype.update = function() {
 
 }
 
-
+// Draw the player on the screen, required method for game ✓
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
@@ -70,7 +80,7 @@ Player.prototype.resetPlayerPosition = function() {
 }
 
 Player.prototype.handleInput = function(keyPressed) {
- 
+    // Key press listener, 'left', 'up', 'right', 'down' ✓
     var stepHorizontalLength = 100;
     var stepVerticalLength = 90;
     this.checkPosition();
@@ -127,10 +137,12 @@ Player.prototype.setHorizontalWallCheckerState = function(leftWallState, rightWa
     this.wallChecker.rightWall = rightWallState;
 }
 
-
+// Now instantiate your objects. ✓
+// Place all enemy objects in an array called allEnemies ✓
+// Place the player object in a variable called player ✓
 var allEnemies = [];
 
-
+// Instantiate all enemies
 for (var i = 0; i < 3; i++) {
     var tempSpeed = Math.floor(Math.random() * 5 + 1) * 75;
     allEnemies.push(new Enemy(-60, 60 + 85 * i, tempSpeed));
@@ -138,7 +150,8 @@ for (var i = 0; i < 3; i++) {
 
 var player = new Player();
 
-
+// This listens for key presses and sends the keys to your ✓
+// Player.handleInput() method. You don't need to modify this. ✓
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
